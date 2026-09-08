2ffa67646bf77a96f26ebf22d37535719cd9c2eb
VoxlarWIP <jamesavalon261@gmail.com>
2026-09-09 02:32:04 +0700

    Update nullsc.lua

--- nullsc.lua
+++ nullsc.lua
@@ -1,3 +1,1 @@
-script_key = "trial"
-
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/f3fc07805fca887cc16602fe6897f8c2c3f4be5ea6d50f7a9e9ee02ac7cf5061.lua"))()
+loadstring(game:HttpGet("https://api.obscuravm.com/scripts/4026801025659135904"))()
