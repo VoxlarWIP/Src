252f89c27f7b59a15f4576111e48a2244d22dab6
VoxlarWIP <jamesavalon261@gmail.com>
2026-09-09 02:29:52 +0700

    Update adustytrip.lua

--- adustytrip.lua
+++ adustytrip.lua
@@ -1,3 +1,1 @@
-script_key = "trial"
-
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/7aa3e413263f3390430331999529b0f68ed6db63a2b6a34d027ff80fd5296a06.lua"))()
+loadstring(game:HttpGet("https://api.obscuravm.com/scripts/4471699920836164626"))()
